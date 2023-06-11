dfff734f28775309de9dc878d203ccffe5ecb915
Hasan Kılınç <hasankilincce@gmail.com>
2023-06-11 15:09:41 +0300

    time range

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 import cv2
 
-def video_to_frame (video_path, output_path):
+min_value = int(input("Başlangıç Saniyesi : "))
+max_value = int(input("Bitiş Saniyesi : "))
+
+def video_to_frame(video_path, output_path, min_value, max_value):
     video = cv2.VideoCapture(video_path)
 
     frame_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
-
+    fps = int(video.get(cv2.CAP_PROP_FPS))
     count = 0
 
     while video.isOpened():
         ret, frame = video.read()
 
         if ret:
-            frame_path = output_path + "/frame{}.jpeg".format(count)
-            cv2.imwrite(frame_path, frame)
+            if fps * min_value <= count <= fps * max_value:
+
+                frame_path = output_path + "/frame{}.jpeg".format(count)
+                cv2.imwrite(frame_path, frame)
+                print("İşlenen görüntü {}/{}".format(count, frame_count))
 
             count += 1
-
-            print("İşlenen görüntü {}/{}".format(count,frame_count))
 
         else:
             break
@@ -25,4 +29,4 @@ def video_to_frame (video_path, output_path):
 
     print("İşlem tamamlandı.")
 
-video_to_frame("istanbul.mp4", "output_path")
+video_to_frame("istanbul.mp4", "output_path", min_value, max_value)
